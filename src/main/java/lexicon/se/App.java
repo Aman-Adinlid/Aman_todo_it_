f8aaedb94254200822a6720952cfb3fab0270452
aman adinlid <95pinkpanda@gmail.com>
2020-12-29 02:37:57 +0100

    testing

--- src/main/java/lexicon/se/App.java
+++ src/main/java/lexicon/se/App.java
@@ -4,16 +4,9 @@ import lexicon.se.model.Person;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        Person person = new Person(12367,"Aman","Adinlid");
-
-        System.out.println(person.getPersonId());
-        System.out.println(person.getFirstName());
-        System.out.println(person.getLastName());
 
 
 
 
     }
-}
+
