a032522ebc09f2d9ff83a06997437215d8d1e0d1
Aman-adinlid <75622358+Aman-adinlid@users.noreply.github.com>
2021-01-03 04:53:01 +0100

    Update TodoSequencer.java

--- src/main/java/lexicon/se/data/TodoSequencer.java
+++ src/main/java/lexicon/se/data/TodoSequencer.java
@@ -3,17 +3,17 @@ package lexicon.se.data;
 public class TodoSequencer {
 
 
-    private static int sequencer = 0;
+    private static int todoId = 0;
 
     public static int NextTodoId(){
 
-        return ++sequencer;
+        return ++todoId;
 
 
     }
 
-    public static void Reset(int sequencer) {
-        sequencer= 0;
+  public static void Reset(int sequencer) {
+        TodoId = 0;
 
     }
 }
